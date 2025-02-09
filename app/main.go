590a8cadbbc8703ade7b895f0175d2ee8e7ab3c5
Klimentin0 <meshkov.klim@gmail.com>
2025-02-09 11:56:15 +0300

    ЕСТЬ ПИНГ МЕЖДУ APP API DB

--- app/main.go
+++ app/main.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/docker/docker/api/types/container"
@@ -23,7 +22,6 @@ type PingResult struct {
 	ContainerName string  `json:"container_name"`
 	PingDuration  float64 `json:"ping_duration"`
 	Status        string  `json:"status"`
-	Service       string  `json:"service"`
 }
 
 // ID этого контейнера
@@ -61,12 +59,13 @@ func discoverContainers() ([]struct {
 	}
 	for _, container := range containers {
 		containerID := container.ID
+		shortID := containerID[:12]
 		containerName := strings.TrimPrefix(container.Names[0], "/")
 		containerList = append(containerList, struct {
 			ID   string
 			Name string
 		}{
-			ID:   containerID,
+			ID:   shortID,
 			Name: containerName,
 		})
 	}
@@ -74,21 +73,20 @@ func discoverContainers() ([]struct {
 }
 
 // пинг через доекровские cli команды
-func pingService(containerID, serviceName string) PingResult {
+func pingService(containerID string) PingResult {
 	startTime := time.Now()
 	thisID := getCurrentContainerID()
 
-	cmd := exec.Command("docker", "exec", "-t", thisID, "ping", "-c", "1", serviceName)
+	cmd := exec.Command("docker", "exec", thisID, "ping", "-c", "1", containerID)
 	err := cmd.Run()
 	duration := time.Since(startTime).Seconds()
 	result := PingResult{
 		ContainerID:  containerID,
 		PingDuration: duration,
 		Status:       "DOWN",
-		Service:      serviceName,
 	}
 	if err != nil {
-		log.Printf("Error pinging service %s from container %s: %v", serviceName, thisID, err)
+		log.Printf("Error pinging container %s: %v", containerID, err)
 		return result
 	}
 	result.Status = "UP"
@@ -99,57 +97,33 @@ func sendPingResult(apiURL string, result PingResult) {
 	jsonData, _ := json.Marshal(result)
 	resp, err := http.Post(apiURL, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
-		fmt.Printf("Failed to send ping result for %s: %v\n", result.Service, err)
+		fmt.Printf("Failed to send ping result for %s: %v\n", result.ContainerID, err)
 		return
 	}
 	defer resp.Body.Close()
-	fmt.Printf("Sent ping result for %s: %s\n %s\n", result.Service, result.Status, result.ContainerID)
+	fmt.Printf("Sent ping result for %s: ID(%s)\n %s\n", result.ContainerName, result.ContainerID, result.Status)
 }
 
-func pingAllContainers(apiURL string, pingedContainers map[string]bool, mutex *sync.Mutex) {
+func pingAllContainers(apiURL string) {
+	var result PingResult
 	containers, err := discoverContainers()
 	if err != nil {
 		log.Printf("Failed to discover containers: %v\n", err)
 		return
 	}
 
-	servicesToPing := []string{"api", "app", "postgres"}
-
 	for _, container := range containers {
-		for _, service := range servicesToPing {
-			mutex.Lock()
-			key := fmt.Sprintf("%s-%s", container.ID, service)
-			if pingedContainers[key] {
-				mutex.Unlock()
-				continue
-			}
-			pingedContainers[key] = true
-			mutex.Unlock()
-
-			result := pingService(container.ID, service)
-			result.ContainerName = container.Name
-			sendPingResult(apiURL, result)
-		}
+		result = pingService(container.ID)
+		sendPingResult(apiURL, result)
 	}
 }
 
 func main() {
 	apiURL := "http://api:8080/ping_results"
 
-	pingedContainers := make(map[string]bool)
-	mutex := &sync.Mutex{}
-
 	for {
-		fmt.Println("Starting ping cycle...")
-		pingAllContainers(apiURL, pingedContainers, mutex)
-
-		mutex.Lock()
-		for key := range pingedContainers {
-			delete(pingedContainers, key)
-		}
-		mutex.Unlock()
-
-		fmt.Println("Ping cycle completed. Waiting for 10 seconds before next cycle.")
+		fmt.Println("Начинаю пинг...")
+		pingAllContainers(apiURL)
 		time.Sleep(10 * time.Second)
 	}
 }
